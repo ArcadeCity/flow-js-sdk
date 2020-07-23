c57e71ac06c2b244e21b0e39938a40ab62d85e7a
Christopher David <chris@arcade.city>
2020-07-23 12:20:09 -0500

    Set gRPC default transport to React Native - sdk calls now work from RN app

--- packages/send/src/unary.js
+++ packages/send/src/unary.js
@@ -1,7 +1,9 @@
 import {grpc} from "@improbable-eng/grpc-web"
-import {NodeHttpTransport} from "@improbable-eng/grpc-web-node-http-transport"
+// import {NodeHttpTransport} from "@improbable-eng/grpc-web-node-http-transport"
+import {ReactNativeTransport} from "@improbable-eng/grpc-web-react-native-transport"
 
-grpc.setDefaultTransport(NodeHttpTransport())
+// https://github.com/improbable-eng/grpc-web/issues/592
+grpc.setDefaultTransport(ReactNativeTransport({}))
 
 export async function unary(host, method, request) {
   return new Promise((resolve, reject) => {
